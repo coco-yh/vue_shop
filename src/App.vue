c6565fa55bab2885ad710f4293c319c76c502709
游浩 <yh_android@163.com>
2020-04-10 11:21:30 +0800

    element 文件导入

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
+<!--
+ * @Author: coco
+ * @Date: 2020-04-10 11:03:32
+ * @LastEditTime: 2020-04-10 11:16:26
+ * @LastEditors: coco
+ * @Description: 
+ * @FilePath: /vue_shop/src/App.vue
+ * @https://github.com/android-coco
+ -->
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'app',
+  components: {}
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,23 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+/*
+ * @Author: coco
+ * @Date: 2020-04-10 11:03:32
+ * @LastEditTime: 2020-04-10 11:20:26
+ * @LastEditors: coco
+ * @Description:
+ * @FilePath: /vue_shop/src/router/index.js
+ * @https://github.com/android-coco
+ */
+import Vue from "vue";
+import VueRouter from "vue-router";
+// import Login from "@/components/login/Login";
+Vue.use(VueRouter);
 
-Vue.use(VueRouter)
-
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+const routes = [
+  // { path: "/login", name: "login", component: Login }
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
